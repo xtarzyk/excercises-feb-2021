2b95becc8cfcc607635a4b8ee9e44de1355344f6
Venebrous <goscin3@gmail.com>
2022-01-08 11:16:08 +0100

    Poms fixed, java properties added

--- multithreading/src/main/java/Multithreading.java
+++ multithreading/src/main/java/Multithreading.java
@@ -1,18 +1,19 @@
 public class Multithreading {
 
+    private static int counter = 0;
+
     public static void main(String[] args) {
 
-    int counter = 0;
 
-    Runnable runnable = () -> counter++;
+        Runnable runnable = () -> counter++;
 
-    Thread thread1 = new Thread(runnable);
-    Thread thread2 = new Thread(runnable);
-    Thread thread3 = new Thread(runnable);
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
 
-    thread1.start();
-    thread2.start();
-    thread3.start();
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
         System.out.println(counter);
 
